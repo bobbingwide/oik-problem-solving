1f94703578f35f7f367549f3db0de257092c8af7
Herb Miller <herb@bobbingwide.com>
2019-10-31 11:27:24 +0000

    First pass definition of question and problem CPTs

---
+++ oik-problem-solving.php
@@ -0,0 +1,102 @@
+<?php
+/*
+Plugin Name: oik-problem-solving
+Plugin URI: https://www.oik-plugins.com/oik-plugins/oik-problem-solving
+Description: Custom Post Types for Problem Solving
+Depends: oik base plugin, oik-fields
+Version: 0.0.0
+Author: bobbingwide
+Author URI: https://www.bobbingwide.com/about-bobbing-wide
+License: GPL2
+
+    Copyright 2019 Bobbing Wide (email : herb@bobbingwide.com )
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 2,
+    as published by the Free Software Foundation.
+
+    You may NOT assume that you can use any other version of the GPL.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    The license for this software can likely be found here:
+    http://www.gnu.org/licenses/gpl-2.0.html
+
+*/
+
+/**
+ * Implement "oik_fields_loaded" action for oik-sites
+ */
+function oik_problem_solving_init( ) {
+	oik_register_questions();
+	oik_register_problems();
+}
+
+/**
+ * Register custom post type "question"
+ *
+ * Fields for the question are
+ * _step_ref - noderef to zero or more _oik_presentation posts
+ */
+function oik_register_questions() {
+	$post_type = 'question';
+	$post_type_args = array();
+	$post_type_args['label'] = 'Questions';
+	$post_type_args['description'] = 'Problem Solving questions. e.g. Has it ever worked?';
+	$post_type_args['hierarchical'] = true;
+	$post_type_args['has_archive'] = true;
+	$post_type_args['supports'] = array( 'title', 'editor', 'excerpt', 'thumbnail', 'revisions', 'page-attributes', 'publicize', 'author' );
+	$post_type_args['show_in_rest'] = true;
+	//$post_type_args = oik_sites_capabilities( $post_type_args );
+	bw_register_post_type( $post_type, $post_type_args );
+	bw_register_field( "_step_ref", "noderef", "Reference",
+		array( '#type' => 'oik_presentation', '#optional' => true, '#multiple' => 34 )  );
+	bw_register_field_for_object_type( "_step_ref", $post_type );
+	add_filter( "manage_edit-${post_type}_columns", "oik_problem_columns", 10, 1 );
+}
+
+/**
+ * Register custom post type "problem"
+ *
+ * Fields for the problem are:
+ * _step_ref - noderef to zero or more _oik_presentation posts
+ *
+ */
+function oik_register_problems() {
+	$post_type = 'problem';
+	$post_type_args = array();
+	$post_type_args['label'] = 'Problems';
+	//$post_type_args['singular_label'] = 'Business';
+	$post_type_args['description'] = 'Examples of Problems associated with a particular step i.e. question';
+	$post_type_args['hierarchical'] = true;
+	$post_type_args['has_archive'] = true;
+	$post_type_args['supports'] = array( 'title', 'editor', 'excerpt', 'thumbnail', 'revisions', 'publicize', 'author' );
+	$post_type_args['show_in_rest'] = true;
+	// Don't think we need capabilities yet
+	bw_register_post_type( $post_type, $post_type_args );
+	bw_register_field_for_object_type( "_step_ref", $post_type );
+	add_filter( "manage_edit-${post_type}_columns", "oik_problem_columns", 10, 1 );
+}
+
+
+/**
+ * Columns to display in the admin page
+ */
+function oik_problem_columns( $columns ) {
+	$columns["_step_ref"] = __( "Step ref" );
+	//bw_trace2();
+	//bw_backtrace();
+	return( $columns );
+}
+
+/**
+ * Function to invoke when oik-sites plugin file is loaded
+ */
+function oik_problem_solving_loaded() {
+	add_action( 'oik_fields_loaded', 'oik_problem_solving_init' );
+}
+
+oik_problem_solving_loaded();
